c07f5bf90d29a0b6d8f6e346260bdaeedc78391b
dalia-gerguis <dodo.gerges22@gmail.com>
2022-02-20 01:50:13 +0200

    create tenants

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Tenant;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -18,8 +20,10 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('123456789'),
+            'role' => 'admin',
             'remember_token' => Str::random(10),
+            'tenant_id' => Tenant::factory()
         ];
     }
 
